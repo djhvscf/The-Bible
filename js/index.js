9b8f46ebaf48047c4649a789dda804d9547cdae1
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-06-11 23:36:35 -0600

    Adding files

---
+++ js/index.js
@@ -0,0 +1,56 @@
+$(function() {
+    $.ajax({
+    url:'http://getbible.net/json',
+    dataType: 'jsonp',
+    data: 'passage=2Jn1:1',
+    jsonp: 'getbible',
+    success:function(json) {
+        // set text direction
+        if (json.direction == 'RTL') {
+        	var direction = 'rtl';
+        } else {
+        	var direction = 'ltr'; 
+        }
+        // check response type
+        if (json.type == 'verse') {
+            var output = '';
+            	$.each(json.book, function(index, value) {
+                	output += '<center><b id="book-name">'+value.book_name+' '+value.chapter_nr+'</b></center><br/><p class="'+ direction +' scripture">';
+                    $.each(value.chapter, function(index, value) {
+                        output += '  <small class="ltr">' +value.verse_nr+ '</small>  ';
+                        output += value.verse;
+                        output += '<br/>';
+                    });
+                    output += '</p>';
+            	});
+            $('#verse').html(output);  // <---- this is the div id we update
+        } else if (json.type == 'chapter') {
+            var output = '<center><b>'+json.book_name+' '+json.chapter_nr+'</b></center><br/><p class="'+direction+'">';
+            $.each(json.chapter, function(index, value) {
+                output += '  <small class="ltr">' +value.verse_nr+ '</small>  ';
+                output += value.verse;
+                output += '<br/>';
+            });
+            output += '</p>';
+            $('#verse').html(output);  // <---- this is the div id we update
+        } else if (json.type == 'book') {
+            var output = '';
+            $.each(json.book, function(index, value) {
+                output += '<center><b>'+json.book_name+' '+value.chapter_nr+'</b></center><br/><p class="'+direction+'">';
+                $.each(value.chapter, function(index, value) {
+                    output += '  <small class="ltr">' +value.verse_nr+ '</small>  ';
+                    output += value.verse;
+                    output += '<br/>';
+                });
+            output += '</p>';
+        });
+        if(addTo) {
+        	$('#verse').html(output);  // <---- this is the div id we update
+        }
+    }
+    },
+    error:function(){
+        $('#verse').html('<h2>No scripture was returned, please try again!</h2>'); // <---- this is the div id we update
+     },
+});  
+});
