8b3f6164317d0890ad39c155bb6992336d2277c3
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-06-14 01:30:03 -0600

    Adding buttons

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
-$(function() {
+var sprintf = function (str) {
+    var args = arguments,
+        flag = true,
+        i = 1;
+
+    str = str.replace(/%s/g, function () {
+        var arg = args[i++];
+
+        if (typeof arg === 'undefined') {
+            flag = false;
+            return '';
+        }
+        return arg;
+    });
+    return flag ? str : '';
+};
+
+var getScriptures = function (random) {
+    $("#bookname").html("");
+    $("#verse").html("Loading verse...");
+    
     $.ajax({
         url:"http://labs.bible.org/api/",
         dataType: "jsonp",
@@ -8,17 +28,31 @@ $(function() {
             "Access-Control-Allow-Methods": "POST,GET,PUT,DELETE",
             "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"
         },
-        data: "passage=random&type=json",
+        data: sprintf("passage=%s&type=json", random ? "random" : "votd"),
         success: function(data) {
             if (data.length > 0) {
-                $("#verse").html(data[0].chapter + ":" + data[0].verse + ". " + data[0].text.split("<a")[0]);
+                $("#verse").html(sprintf("<b>%s:%s</b> %s", data[0].chapter, data[0].verse, data[0].text.split("<a")[0]));
                 $("#bookname").html(data[0].bookname);
             } else {
-                $("#verse").html("<h2>No scripture was returned, please try again!</h2>");
+                $("#verse").html(sprintf("<h2>%s</h2>", "No scripture was returned, please try again!"));
+                $("#bookname").html("");
             }
         },
         error:function() {
-            $("#verse").html("<h2>No scripture was returned, please try again!</h2>");
+            $("#verse").html(sprintf("<h2>%s</h2>", "No scripture was returned, please try again!"));
+            $("#bookname").html("");
         },
-    }); 
+    });
+};
+
+$(function() {
+    getScriptures(true);
+
+    $("#daily-verse").click(function () {
+        getScriptures(false);
+    });
+
+    $("#random-verse").click(function () {
+        getScriptures(true);
+    });
 });
